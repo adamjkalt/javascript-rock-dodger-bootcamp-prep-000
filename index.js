a25143afcc9157e336d00d7f105cc1d4b0ad4481
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-26 03:02:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,7 +122,6 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  //clear gameInterval
   for (i = 0; i < ROCKS.length; i++) {
     ROCKS.remove();
   }
